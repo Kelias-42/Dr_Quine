8b7d8d0b7ad5a33c1ea704617813dbe11c563f25
MBA-Aleksi <aleksi.gautier@gmail.com>
2020-05-03 17:28:23 +0200

    completed C quine

--- C/colleen.c
+++ C/colleen.c
@@ -17,6 +17,6 @@ int main(void)
 	I also like airplanes
 */
 	fnc();
-	char *prog = "#include <stdio.h>%c%c/*%c%cI like trains%c*/%c%c static char c[] = %c%cn%c%c%c%c%ct%c;%c%cvoid fnc()%c{";
-	printf(prog, c[0], c[0], c[0], c[3], c[0], c[0], c[0], c[1], c[2], c[2], c[1], c[2], c[2], c[2], c[1], c[0], c[0], c[0], prog);
+	char *prog = "#include <stdio.h>%c%c/*%c%cI like trains%c*/%c%cstatic char c[] = %c%cn%c%c%c%c%ct%c;%c%cvoid fnc()%c{%c%cint a = 1;%c}%c%cint main(void)%c{%c/*%c%cI also like airplanes%c*/%c%cfnc();%c%cchar *prog = %c%s%c;%c%cprintf(prog, c[0], c[0], c[0], c[3], c[0], c[0], c[0], c[1], c[2], c[2], c[1], c[2], c[2], c[2], c[1], c[0], c[0], c[0],c[0], c[3], c[0], c[0], c[0], c[0], c[0], c[0], c[3], c[0],c[0], c[3], c[0], c[3], c[1], prog, c[1], c[0], c[3], c[0], c[0]);%c}%c";
+	printf(prog, c[0], c[0], c[0], c[3], c[0], c[0], c[0], c[1], c[2], c[2], c[1], c[2], c[2], c[2], c[1], c[0], c[0], c[0],c[0], c[3], c[0], c[0], c[0], c[0], c[0], c[0], c[3], c[0],c[0], c[3], c[0], c[3], c[1], prog, c[1], c[0], c[3], c[0], c[0]);
 }
