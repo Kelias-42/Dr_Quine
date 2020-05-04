47280a0fbc3bf250282e193de73ef1756e22232d
MBA-Aleksi <aleksi.gautier@gmail.com>
2020-05-04 11:48:54 +0200

    fixed formatting

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #define C "\n\"\\\t"
-#define PROG "#include <stdio.h>%c#define C %c%cn%c%c%c%c%ct%c%c#define PROG %c%s%c%c#define MAIN(VAR) int main(void){FILE *fd = fopen(VAR, %cw%c);fprintf(fd, PROG, C[0], C[1], C[2], C[2], C[1], C[2], C[2], C[2], C[1], C[0], C[1], PROG, C[1], C[0], C[1], C[1], C[0], C[0], C[3], C[0], C[0], C[1], C[1], C[0]);fclose(fd);return (0);}%c/*%c%cHello, this is a self-replicating comment%c*/%cMAIN(%cGrace_kid.c%c)%c"
-#define MAIN(VAR) int main(void){FILE *fd = fopen(VAR, "w");fprintf(fd, PROG, C[0], C[1], C[2], C[2], C[1], C[2], C[2], C[2], C[1], C[0], C[1], PROG, C[1], C[0], C[1], C[1], C[0], C[0], C[3], C[0], C[0], C[1], C[1], C[0]);fclose(fd);return (0);}
+#define PROG "#include <stdio.h>%c#define C %c%cn%c%c%c%c%ct%c%c#define PROG %c%s%c%c#define FT(x) int main(){FILE *fd = fopen(x, %cw%c);fprintf(fd, PROG, C[0], C[1], C[2], C[2], C[1], C[2], C[2], C[2], C[1], C[0], C[1], PROG, C[1], C[0], C[1], C[1], C[0], C[0], C[3], C[0], C[0], C[1], C[1], C[0]);fclose(fd);return (0);}%c/*%c%cHello, this is a self-replicating comment%c*/%cFT(%cGrace_kid.c%c)%c"
+#define FT(x) int main(){FILE *fd = fopen(x, "w");fprintf(fd, PROG, C[0], C[1], C[2], C[2], C[1], C[2], C[2], C[2], C[1], C[0], C[1], PROG, C[1], C[0], C[1], C[1], C[0], C[0], C[3], C[0], C[0], C[1], C[1], C[0]);fclose(fd);return (0);}
 /*
 	Hello, this is a self-replicating comment
 */
-MAIN("Grace_kid.c")
+FT("Grace_kid.c")
