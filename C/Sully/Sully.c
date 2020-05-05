aceffbd713e55aae1f812791695fbe13dd1a1c65
MBA-Aleksi <aleksi.gautier@gmail.com>
2020-05-05 12:12:39 +0200

    added almost-working version of Sully

--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -10,12 +10,17 @@ int main(void)
 
 	if (--i < 0)
 		return (0);
-	char *code = "#include <stdio.h>%c#include <stdlib.h>%c%cstatic char c[] = %c%cn%c%c%c%c%ct%c;%c%cint main(void)%c{%c%cint i = %d;%c%cFILE *fd;%c%c%cif (--i < 0)%c%c%creturn (0);%c%cchar *code = %c%s%c;%c%cchar fname[] = %cSully_x.c%c;%c%cfname[6] = i + '0';%c%cif (!(fd = fopen(fname, %cw%c)))%c%c%creturn (-1);%c";
+	char *code = "#include <stdio.h>%c#include <stdlib.h>%c%cstatic char c[] = %c%cn%c%c%c%c%ct%c;%c%cint main(void)%c{%c%cint i = %d;%c%cFILE *fd;%c%c%cif (--i < 0)%c%c%creturn (0);%c%cchar *code = %c%s%c;%c%cchar fname[] = %cSully_x.c%c;%c%cfname[6] = i + '0';%c%cif (!(fd = fopen(fname, %cw%c)))%c%c%creturn (-1);%c%cfprintf(fd, code, c[0], c[0], c[0], c[1], c[2], c[2], c[1], c[2], c[2], c[2], c[1], c[0], c[0], c[0], c[0], c[3], i, c[0], c[3], c[0], c[0], c[3], c[0], c[3], c[3], c[0], c[3], c[1], code, c[1], c[0], c[3], c[1], c[1], c[0], c[3], c[0], c[3], c[1], c[1], c[0], c[3], c[3], c[0], c[3], c[0], c[3], c[0], c[3], c[1], c[1], c[0], c[3], c[0], c[3], c[0], c[3], c[0], c[3], c[0], c[3], c[0], c[0]);%c%cfclose(fd);%c%cchar cmp[] = %cclang -Wall -Wextra -Werror Sully_x.c -o Sully_x; ./Sully_x%c;%c%ccmp[34] = i + '0';%c%ccmp[47] = i + '0';%c%ccmp[58] = i + '0';%c%csystem(cmp);%c%creturn (0);%c}%c";
 	char fname[] = "Sully_x.c";
 	fname[6] = i + '0';
 	if (!(fd = fopen(fname, "w")))
 		return (-1);
-	fprintf(fd, code, c[0], c[0], c[0], c[1], c[2], c[2], c[1], c[2], c[2], c[2], c[1], c[0], c[0], c[0], c[0], c[3], i, c[0], c[3], c[0], c[0], c[3], c[0], c[3], c[3], c[0], c[3], c[1], code, c[1], c[0], c[3], c[1], c[1], c[0], c[3], c[0], c[3], c[1], c[1], c[0], c[3], c[3], c[0]);
+	fprintf(fd, code, c[0], c[0], c[0], c[1], c[2], c[2], c[1], c[2], c[2], c[2], c[1], c[0], c[0], c[0], c[0], c[3], i, c[0], c[3], c[0], c[0], c[3], c[0], c[3], c[3], c[0], c[3], c[1], code, c[1], c[0], c[3], c[1], c[1], c[0], c[3], c[0], c[3], c[1], c[1], c[0], c[3], c[3], c[0], c[3], c[0], c[3], c[0], c[3], c[1], c[1], c[0], c[3], c[0], c[3], c[0], c[3], c[0], c[3], c[0], c[3], c[0], c[0]);
 	fclose(fd);
+	char cmp[] = "clang -Wall -Wextra -Werror Sully_x.c -o Sully_x; ./Sully_x";
+	cmp[34] = i + '0';
+	cmp[47] = i + '0';
+	cmp[58] = i + '0';
+	system(cmp);
 	return (0);
 }
