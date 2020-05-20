47c8df060eef171380f6ab74e1854a25abcdb863
MBA-Aleksi <aleksi.gautier@gmail.com>
2020-05-20 22:14:37 +0200

    fixed minor mistake

--- Python/Colleen/Colleen.py
+++ Python/Colleen/Colleen.py
@@ -8,5 +8,5 @@ def fnc():
 if __name__ == "__main__":
 	fnc()
 	# Yep, still a Quine
-	code = "# This is a Quine %c%cc = %c%cn%c%c%c%c%ct%c%c%c%cdef fnc():%c%creturn%c%cif __name__ == %c__main__%c:%c%cfnc()%c%c# Yep, still a Quine%c%ccode = %c%s%c%c%cprint(code %c (c[0], c[0], c[1], c[2], c[2], c[1], c[2], c[2], c[2], c[4], c[1], c[0], c[0], c[0], c[3], c[0], c[0], c[1], c[1], c[0], c[3], c[0], c[3], c[0], c[3], c[1], code, c[1], c[0], c[3], c[4]))"
+	code = "# This is a Quine%c%cc = %c%cn%c%c%c%c%ct%c%c%c%cdef fnc():%c%creturn%c%cif __name__ == %c__main__%c:%c%cfnc()%c%c# Yep, still a Quine%c%ccode = %c%s%c%c%cprint(code %c (c[0], c[0], c[1], c[2], c[2], c[1], c[2], c[2], c[2], c[4], c[1], c[0], c[0], c[0], c[3], c[0], c[0], c[1], c[1], c[0], c[3], c[0], c[3], c[0], c[3], c[1], code, c[1], c[0], c[3], c[4]))"
 	print(code % (c[0], c[0], c[1], c[2], c[2], c[1], c[2], c[2], c[2], c[4], c[1], c[0], c[0], c[0], c[3], c[0], c[0], c[1], c[1], c[0], c[3], c[0], c[3], c[0], c[3], c[1], code, c[1], c[0], c[3], c[4]))
